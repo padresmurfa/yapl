088b84924f25c785705d1891a84e859ec01efc8f
David Brandt <davidbrandt@asana.com>
2020-06-26 15:20:17 +0000

    cp

--- v1/2_modules_from_package/cli.py
+++ v1/2_modules_from_package/cli.py
@@ -2,14 +2,9 @@
 import argparse
 import sys
 import os
-import time
-from datetime import datetime
-import shutil
 from pathlib import Path
-import yapl.repository.git.lib as git
 import json
 import io
-import yapl
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
@@ -23,24 +18,43 @@ def error(msg):
 def progress(msg):
     print("[PROGRESS] " + msg)
 
-def get_package_filename(parsed_args):
-    return str(Path(parsed_args.build) / "packages" / (parsed_args.package + ".json"))
+def get_package_filename(build, package):
+    return str(Path(build) / "packages" / (package + ".json"))
+
+def get_source_filename(build, source):
+    return str(Path(build) / "files" / (source + ".yapl"))
 
 def validate_parsed_args(parsed_args):
-    if not os.path.isfile(get_package_filename(parsed_args)):
+    if not os.path.isfile(get_package_filename(parsed_args.build, parsed_args.package)):
         error("The parameters must specify a package file")
         return 1
     return 0
 
 def main(parsed_args):
-    package_filename = get_package_filename(parsed_args)
     progress("Starting")
     progress("Preparing output directories")
     modules_path = Path(parsed_args.build) / "modules"
-    Path(modules_path).mkdir(parents=True, exist_ok=True)
+    modules_path.mkdir(parents=True, exist_ok=True)
+    process_package(parsed_args.build, parsed_args.package)
     progress("Finished")
     return 0
 
+def load_package(build, package);
+    package_filename = get_package_filename(build, package)
+    with io.open(package_filename) as json_file:
+        return json.load(json_file)
+
+def load_sourcefile(build, hash);
+    source_filename = get_source_filename(build, hash)
+    with io.open(source_filename) as yapl_file:
+        return yapl_file.read()
+
+def process_package(build, package):
+    progress("Processing package " + parsed_args.package)
+    descriptor = load_package(build, package)
+    for source_filename, sourcefile_info in descriptor["source_files"].items():
+        revision = sourcefile_info["revision"]
+        source_file = load_sourcefile(build, revision)
 
 if __name__ == "__main__":
     parsed_args = parse_args(sys.argv[1:])
